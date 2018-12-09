319d08635818ccf7d9987e7504f1ecec5391d493
EffyChu <43943757+EffyChu@users.noreply.github.com>
2018-12-09 16:30:52 -0600

    Update Import.java

--- Import.java
+++ Import.java
@@ -7,30 +7,37 @@ import javafx.scene.control.*;
 import javafx.scene.layout.*;
 import javafx.geometry.*;
 
+/**
+* This class decribes the import function
+**/
 public class Import {
-
+	/**
+	* This method set the display the import window
+	*
+	* @param the lists of food data
+	**/
 	public void display(FoodData list) {
 		Stage popupwindow = new Stage();
 		popupwindow.initModality(Modality.APPLICATION_MODAL); // make sure only be able to work with this current window
-		popupwindow.setTitle("Import");
-		popupwindow.setWidth(400);
-		popupwindow.setHeight(200);
+		popupwindow.setTitle("Import"); // title of the window
+		popupwindow.setWidth(400); 
+		popupwindow.setHeight(200); // size of the window 
 
-		BorderPane bp = new BorderPane();
-		Insets insets = new Insets(10, 10, 10, 10);
-		bp.setPadding(insets);
+		BorderPane bp = new BorderPane(); // create a new borderpane
+		Insets insets = new Insets(10, 10, 10, 10); 
+		bp.setPadding(insets); // set padding of the window
 
-		Label filepath = new Label("File Path:");
-		filepath.setStyle("-fx-font-size: 14px;");
+		Label filepath = new Label("File Path:"); // create filepath label
+		filepath.setStyle("-fx-font-size: 14px;"); // set label display
 
 		TextField t1field = new TextField("src/application/foodItems.csv");
-		t1field.setPrefWidth(350);
+		t1field.setPrefWidth(350); 
 
 		Label msg = new Label();
 
-		Button back = new Button("Back");
-		back.setOnAction(e -> popupwindow.close());
-		Button impbutton = new Button("Import");
+		Button back = new Button("Back"); // create a back button
+		back.setOnAction(e -> popupwindow.close()); // set the action of the back button
+		Button impbutton = new Button("Import"); // create a impbutton button
 		impbutton.setOnAction(e -> {
 			list.loadFoodItems(t1field.getText().trim());
 			if (list.getStatusError()) {
@@ -39,18 +46,18 @@ public class Import {
 			} else {
 				popupwindow.close();
 			}
-		});
+		}); // set the action of impbutton button
 
-		HBox hbox = new HBox();
-		hbox.setSpacing(10);
-		hbox.getChildren().addAll(back, impbutton);
-		hbox.setAlignment(Pos.BOTTOM_RIGHT);
+		HBox hbox = new HBox(); // create a new hbox
+		hbox.setSpacing(10); // set spacing
+		hbox.getChildren().addAll(back, impbutton); // add buttons to display
+		hbox.setAlignment(Pos.BOTTOM_RIGHT); // set the postion of the hbox
 
-		VBox vbox = new VBox();
-		vbox.getChildren().addAll(filepath, t1field, msg);
+		VBox vbox = new VBox(); // create a new vbox
+		vbox.getChildren().addAll(filepath, t1field, msg); // add filepath, t1field and msg to display
 
-		bp.setLeft(vbox);
-		bp.setBottom(hbox);
+		bp.setLeft(vbox); 
+		bp.setBottom(hbox); // set position
 
 		Scene scene1 = new Scene(bp, 500, 300);
 		popupwindow.setScene(scene1);
