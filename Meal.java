dcc8ab845f2fa259a50d1ce19ea00f2d6965c2e7
bthern <33269432+bthern@users.noreply.github.com>
2018-12-11 21:06:58 -0600

    Comment

--- Meal.java
+++ Meal.java
@@ -1,3 +1,18 @@
+/**
+ * Filename:   Main.java
+ * Project:    Food Query and Meal Analysis
+ * Version:    1.0
+ * Date:       Nov 29th, 2018
+ * Authors:    Anapat Chairithinugull, Brock Thern, Effy Chu, Zening Fang
+ *
+ * Semester:   Fall 2018
+ * Course:     CS400
+ * Instructor: Deppeler (deppeler@cs.wisc.edu)
+ * Credits:    
+ * Bugs:       
+ *
+ * Due Date:   before 10:00 pm on November 30th
+ */
 package application;
 
 import java.util.ArrayList;
